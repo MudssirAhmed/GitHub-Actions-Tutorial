db8a533d198477478792d153fb67c82dee923faf
MudssirAhmed <mudssira01@gmail.com>
2023-07-27 23:11:16 +0530

    sonarqube added

--- app/src/main/java/com/example/githubactionstutorial/MainActivity.kt
+++ app/src/main/java/com/example/githubactionstutorial/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // c3aec00ae8ff9d3d9b63ded49d256fab61d12fdd
     }
 }
